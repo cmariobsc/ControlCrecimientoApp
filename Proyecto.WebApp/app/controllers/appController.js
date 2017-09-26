252e16f7e0d435db5ea7879873bd041dc2339747
carlos.gonzalez <carlos.gonzalez@IPRC-0200>
2017-09-25 22:35:26 -0500

    Inclusión de video

--- Proyecto.WebApp/app/controllers/appController.js
+++ Proyecto.WebApp/app/controllers/appController.js
@@ -13,5 +13,41 @@ proyectoApp.controller('appController',
                 $location.path('/app');
                 $scope.appActive = true;
             }
+
+            $scope.calculaIMC = function () {
+                var alturaCuadrado = Math.pow($scope.datoChildren.talla / 100, 2);
+                var imc = $scope.datoChildren.peso / alturaCuadrado;
+                $scope.datoChildren.imc = roundToTwo(imc);
+
+                /*Calculo descripcion IMC*/
+                if (imc < 16) {
+                    $scope.datoChildren.detalleIMC = "Delgadez Severa";
+                }
+                else if (imc < 17) {
+                    $scope.datoChildren.detalleIMC = "Delgadez Moderada";
+                }
+                else if (imc < 18.5) {
+                    $scope.datoChildren.detalleIMC = "Delgadez Aceptable";
+                }
+                else if (imc < 25) {
+                    $scope.datoChildren.detalleIMC = "Peso Normal";
+                }
+                else if (imc < 30) {
+                    $scope.datoChildren.detalleIMC = "Sobrepeso";
+                }
+                else if (imc < 35) {
+                    $scope.datoChildren.detalleIMC = "Obeso: Tipo I";
+                }
+                else if (imc < 40) {
+                    $scope.datoChildren.detalleIMC = "Obeso: Tipo II";
+                }
+                else if (imc >= 40) {
+                    $scope.datoChildren.detalleIMC = "Obeso: Tipo III";
+                }
+            }
+
+            function roundToTwo(num) {
+                return +(Math.round(num + "e+2") + "e-2");
+            }
         }
     ]);
